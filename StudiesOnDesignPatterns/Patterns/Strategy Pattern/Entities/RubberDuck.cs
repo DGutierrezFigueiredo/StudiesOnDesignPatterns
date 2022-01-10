e24c884ee79ac5f83c8581814a002c7ad8a42fb6
Diego Gutierrez <diegogutierrezfigueiredo@gmail.com>
2022-01-10 14:03:06 -0300

    rubber duck

---
+++ StudiesOnDesignPatterns/Patterns/Strategy Pattern/Entities/RubberDuck.cs
@@ -0,0 +1,22 @@
+﻿using StudiesOnDesignPatterns.Patterns.Strategy_Pattern.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudiesOnDesignPatterns.Patterns.Strategy_Pattern.Entities
+{
+    public class RubberDuck : Duck
+    {
+        public RubberDuck()
+        {
+            _flyBehavior = new FlyNoWay();
+            _quackBehavior = new QuackNoWay();
+        }
+        public override void Display()
+        {
+            Console.WriteLine("I'm a rubber Duck and I love bubble baths!"); 
+        }
+    }
+}
--- StudiesOnDesignPatterns/Program.cs
+++ StudiesOnDesignPatterns/Program.cs
@@ -9,9 +9,19 @@ namespace StudiesOnDesignPatterns
         static void Main(string[] args)
         {
 
+            Console.Write("Mallard Duck test");
+
             Duck mallard = new MallardDuck();
             mallard.PerformFly();
             mallard.PerformQuack();
+
+            Console.WriteLine();
+
+            Console.Write("Rubber Duck test");
+
+            Duck rubber = new RubberDuck();
+            rubber.PerformFly();
+            rubber.PerformQuack();
 
             Console.ReadKey();
             //The different patterns are stored in the 'Patterns' folder
