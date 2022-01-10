355845a91e7360ad75475155d5e48a3a09501ba6
Diego Gutierrez <diegogutierrezfigueiredo@gmail.com>
2022-01-10 14:07:23 -0300

    2 ducks

--- StudiesOnDesignPatterns/Program.cs
+++ StudiesOnDesignPatterns/Program.cs
@@ -9,19 +9,23 @@ namespace StudiesOnDesignPatterns
         static void Main(string[] args)
         {
 
-            Console.Write("Mallard Duck test");
+            Console.WriteLine("Mallard Duck test");
 
             Duck mallard = new MallardDuck();
             mallard.PerformFly();
             mallard.PerformQuack();
+            mallard.Swim();
+            mallard.Display();
 
             Console.WriteLine();
 
-            Console.Write("Rubber Duck test");
+            Console.WriteLine("Rubber Duck test");
 
             Duck rubber = new RubberDuck();
             rubber.PerformFly();
             rubber.PerformQuack();
+            rubber.Swim();
+            rubber.Display();
 
             Console.ReadKey();
             //The different patterns are stored in the 'Patterns' folder
